f22f8e0cadf30eae258c82c146356dddd9f54830
CareuT <carrotnostik@gmail.com>
2019-07-15 21:59:02 -0700

    Replaced MyApp with Rockstable front page

--- index.tsx
+++ index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import LandingPage from './src/js/components/LandingPage';
 import MyApp from './src/js/components/MyApp';
 import './src/resources/css/sitestyles.scss';
 
-ReactDOM.render(<MyApp />, document.getElementById('root'));
+ReactDOM.render(<LandingPage />, document.getElementById('root'));
